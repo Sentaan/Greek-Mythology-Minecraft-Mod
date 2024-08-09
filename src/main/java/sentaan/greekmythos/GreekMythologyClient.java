1dc407ed7bf5854e36f4dc3165bd7b74eaaaa05d
Adam <sentaanyt@gmail.com>
2024-08-09 17:58:23 +0100

    Followed some of KaupenJoe's 1.21.X mod setup tutorial video

---
+++ src/main/java/sentaan/greekmythos/GreekMythologyClient.java
@@ -0,0 +1,11 @@
+package sentaan.greekmythos;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class GreekMythologyClient implements ClientModInitializer
+{
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
